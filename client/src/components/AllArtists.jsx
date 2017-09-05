5eb2597b8b3686801ff79a6099df4df6cceeb57c
Jess Gardner <inanna1130@gmail.com>
2017-09-05 14:48:05 -0400

    added components, styled GlobalNav

---
+++ client/src/components/AllArtists.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class AllArtists extends Component {
+    render(){
+        return (
+            <div>
+                <h2>hello from all artists</h2>
+            </div>
+        )
+    }
+}
+
+export default AllArtists;
---
+++ client/src/components/Artist.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Artist extends Component {
+    render(){
+        return (
+            <div>
+                <h2>hello from single artist</h2>
+            </div>
+        )
+    }
+}
+
+export default Artist;
